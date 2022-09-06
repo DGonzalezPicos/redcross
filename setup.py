71a89d1ac3f9864a01ba51d072ce72b429cd25fb
Dario <dariogonzalezpicos@gmail.com>
2022-09-06 11:00:05 +0200

    Upgrade to v0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='redcross',
-      version='0.0.1',
+      version='0.0.2',
       description='Reduction and Cross-Correlation of High-Res Spectroscopy',
       packages=['redcross'],
       author_email='dariogonzalezpicos@gmail.com',
